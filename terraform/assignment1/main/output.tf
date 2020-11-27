96c5634e31028704a3f35d7e2c7b0f5e6e8ecd49
Erlend Solbakken Nikolaisen <erls@stud.ntnu.no>
2020-11-27 16:07:59 +0100

    fddf

--- terraform/assignment1/main/output.tf
+++ terraform/assignment1/main/output.tf
@@ -5,3 +5,7 @@ output "public_ip" {
 output "public_ip2" {
     value = google_compute_address.webserver.*.address
 }
+
+output "public_ip3" {
+    value = google_compute_instance.webserver.*.network_interface[0].access_config[0].nat_ip
+}
